79d25f3bc9c75e7c547b9efc59107f2ddbe78ea6
adavia <andresi.davia@gmail.com>
2016-02-10 00:21:05 -0500

    Added Devise + sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root "events#index"
 
+  devise_for :users
+
   resources :events
 
   # Example of regular route:
