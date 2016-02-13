12a498cdb9a92b63b74f42003f7375f3f09e2aaf
adavia <andresi.davia@gmail.com>
2016-02-13 11:43:37 -0500

    Improve attendances test

--- spec/features/joining_attendees_spec.rb
+++ spec/features/joining_attendees_spec.rb
@@ -15,5 +15,9 @@ RSpec.feature "Users can join events" do
     click_link "Pool Party in forest"
     click_link "Join event"
     expect(page).to have_content "Your request has been sent to join this event."
+    expect(page).to have_content "Cancel joined request"
+    within(:css, "ul.event-requests", match: :first) do
+      expect(page).to have_content user.email
+    end
   end
 end
