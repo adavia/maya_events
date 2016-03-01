f5b7bfdebed8135249fe662e3dcee2c29b300835
Andrés Da Viá <andresi.davia@gmail.com>
2016-03-01 09:18:07 -0600

    Change linked files in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,12 +23,12 @@ set :deploy_to, '/home/adavia/maya_events'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
